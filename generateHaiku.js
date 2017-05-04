8cd17e4d7539f37dcaee807b8f2ad6586fe204d8
lc9900 <whynot_hm@hotmail.com>
2017-05-04 16:40:12 -0400

    Added more complex structure to test

--- generateHaiku.js
+++ generateHaiku.js
@@ -6,6 +6,11 @@ var parseDict = require('./parseDict');
 
 var cmudictFile = parseDict.readCmudictFile('./cmudict.txt');
 var data = parseDict.formatData(cmudictFile);
-var struct = [[5],[7],[5]];
+// var struct = [[5],[7],[5]];
+var struct = [
+  [2,3],
+  [1,3,3],
+  [3,2]
+];
 
 console.log(makeHaiku.createHaiku(struct, data.byCount));
